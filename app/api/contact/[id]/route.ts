d6328b78e29c1755884be0b2b6ea19dd19a6e56c
Markian <_email@gmail.com>
2026-07-18 23:55:08 +0300

    Fix delete API route

---
+++ app/api/contact/[id]/route.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params;
+
+    await prisma.contactMessage.delete({
+      where: {
+        id,
+      },
+    });
+
+    return NextResponse.json({
+      success: true,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        success: false,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
